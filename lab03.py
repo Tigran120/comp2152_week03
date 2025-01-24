34ef5d9cce350f6965f528c3f744483c73804765
Tigran120 <Tigrankhachaturyan@georgebrown.ca>
2025-01-24 09:29:13 -0500

    update to dice game

--- lab03.py
+++ lab03.py
@@ -1,1 +1,7 @@
-print("Hello, World")
+diceOptions=list(range(1,7))
+
+#Weapons Array
+weapons=["Fist", "Knife", "Club", "Gun", "Bomb", "Nuclear Bomb"]
+
+#Display available weapons
+print("Available options:", ','.join(weapons))
